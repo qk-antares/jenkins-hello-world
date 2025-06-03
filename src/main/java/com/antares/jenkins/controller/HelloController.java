192f22e874d1d0af710ea0728b39ca082243c9fc
antares <1716607668@qq.com>
2025-06-03 14:58:35 +0800

    更新接口响应

--- src/main/java/com/antares/jenkins/controller/HelloController.java
+++ src/main/java/com/antares/jenkins/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Jenkins updated updated!";
+        return "Hello, Jenkins updated!";
     }
 }
